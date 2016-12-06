44d31d4c877e8628484b9b5f7a1923ff08c86700
Dabrowski <dabrowsp@emea.roche.com>
2016-12-06 09:37:59 +0100

    day 6

---
+++ 2016/advent6_2.py
@@ -0,0 +1,12 @@
+import operator
+
+f = open("input6.txt", 'r')
+lines = f.read().splitlines()
+f.close()
+
+resultMap = [{} for i in lines[0]]
+for line in lines:
+    for letter, d in zip(line, resultMap):
+        d[letter] = d.get(letter, 0) + 1
+
+print ''.join(list(min(element.iteritems(), key=operator.itemgetter(1))[0] for element in resultMap))
